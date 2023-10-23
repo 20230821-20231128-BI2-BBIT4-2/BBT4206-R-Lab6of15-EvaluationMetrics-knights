006ae02217a0470a748795328208b17d66a1f653
PeterDavid3800 <davidpeter487@gmail.com>
2023-10-23 23:13:27 +0300

    Commit

--- Lab6-Submission-EvaluationMetrics.R
+++ Lab6-Submission-EvaluationMetrics.R
@@ -218,56 +218,7 @@ crop_model_cart <- train(label ~ ., data = Crop_recommendation, method = "rpart"
 
 print(crop_model_cart)
 
-# **Required Lab Work Submission** ----
-## Part A ----
-# Create a new file called
-# "Lab6-Submission-EvaluationMetrics.R".
-# Provide all the code you have used to demonstrate the classification and
-# regression evaluation metrics we have gone through in this lab.
-# This should be done on any datasets of your choice except the ones used in
-# this lab.
-
-## Part B ----
-# Upload *the link* to your
-# "Lab6-Submission-EvaluationMetrics.R" hosted
-# on Github (do not upload the .R file itself) through the submission link
-# provided on eLearning.
-
-## Part C ----
-# Create a markdown file called "Lab-Submission-Markdown.Rmd"
-# and place it inside the folder called "markdown". Use R Studio to ensure the
-# .Rmd file is based on the "GitHub Document (Markdown)" template when it is
-# being created.
-
-# Refer to the following file in Lab 1 for an example of a .Rmd file based on
-# the "GitHub Document (Markdown)" template:
-#     https://github.com/course-files/BBT4206-R-Lab1of15-LoadingDatasets/blob/main/markdown/BIProject-Template.Rmd # nolint
-
-# Include Line 1 to 14 of BIProject-Template.Rmd in your .Rmd file to make it
-# displayable on GitHub when rendered into its .md version
-
-# It should have code chunks that explain all the steps performed on the
-# datasets.
-
-## Part D ----
-# Render the .Rmd (R markdown) file into its .md (markdown) version by using
-# knitR in RStudio.
-
-# You need to download and install "pandoc" to render the R markdown.
-# Pandoc is a file converter that can be used to convert the following files:
-#   https://pandoc.org/diagram.svgz?v=20230831075849
-
-# Documentation:
-#   https://pandoc.org/installing.html and
-#   https://github.com/REditorSupport/vscode-R/wiki/R-Markdown
-
-# By default, Rmd files are open as Markdown documents. To enable R Markdown
-# features, you need to associate *.Rmd files with rmd language.
-# Add an entry Item "*.Rmd" and Value "rmd" in the VS Code settings,
-# "File Association" option.
-
-# Documentation of knitR: https://www.rdocumentation.org/packages/knitr/
-
-# Upload *the link* to "Lab-Submission-Markdown.md" (not .Rmd)
-# markdown file hosted on Github (do not upload the .Rmd or .md markdown files)
-# through the submission link provided on eLearning.
+# References ----
+##UCI MACHINE LEARNING. (2016). Adult Census Income. Predict whether income exceeds $50K/yr based on census data. Retrieved from: https://www.kaggle.com/datasets/uciml/adult-census-income
+##Yasser H, M. (2021). Student Marks Dataset. Student Marks Prediction - Regression Problem. Retrieved from: https://www.kaggle.com/datasets/atharvaingle/crop-recommendation-dataset/
+##Ingle, Atharva. (2020). Crop Recommendation Dataset. Maximize agricultural yield by recommending appropriate crops. Retrieved from: https://www.kaggle.com/datasets/atharvaingle/crop-recommendation-dataset/
